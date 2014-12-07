6f4ed2a4f9053786622b1fa46ed0f8f45c50a206
Andy Perlitch <andyperlitch@gmail.com>
2014-12-06 18:08:33 -0800

    Added SpriteChanger scripts, converted to prefab

---
+++ Assets/Scripts/SpriteChanger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteChanger : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		byte[] image = System.IO.File.ReadAllBytes ("Assets/Sprites/charlie.png");
+		SpriteRenderer sprite = GetComponent<SpriteRenderer> ();
+		sprite.sprite.texture.LoadImage (image);
+
+	}
+
+}

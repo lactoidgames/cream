e59303bc3efeae7a96582fef760eb3025d0793af
Andy Perlitch <andyperlitch@gmail.com>
2014-12-21 15:26:53 -0800

    added dynamic loading of character image

--- Assets/Scripts/SpriteChanger.cs
+++ Assets/Scripts/SpriteChanger.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class SpriteChanger : MonoBehaviour {
 
-
+	public string avatar;
 
 	// Use this for initialization
 	void Start () {
-		byte[] image = System.IO.File.ReadAllBytes ("Assets/Sprites/charlie.png");
+		byte[] image = System.IO.File.ReadAllBytes ("Assets/Sprites/" + avatar + ".png");
 		SpriteRenderer sprite = GetComponent<SpriteRenderer> ();
 		sprite.sprite.texture.LoadImage (image);
 

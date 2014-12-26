c32bca81f2158052d0595cbbdf1d0bb7987ad706
Andy Perlitch <andyperlitch@gmail.com>
2014-12-26 13:05:02 -0800

    set andrew as default avatar in Main scene, added (unused) SetCameraSize script for later

---
+++ Assets/Scripts/SetCameraSize.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetCameraSize : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Screen.width: " + Screen.width);
+		Debug.Log ("Screen.height: " + Screen.height);
+		Debug.Log (camera);
+	}
+
+}

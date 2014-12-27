58711e827313a6d9fd87101aeeb5dbbf0d02b511
Ben Holyomes <bholyomes@gmail.com>
2014-12-27 12:21:45 -0800

    issue #3 - created basic coin artwork, made it a prefab. created game control object with script to instantiate the coin prefab randomly on game start and while game is running.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject coin;
+		
+	void Start ()
+	{
+		spawnCoin (200);
+		StartCoroutine (CoinGeneration ());
+	}
+	
+	IEnumerator CoinGeneration ()
+	{
+		
+		while (true)
+		{
+			spawnCoin(1);
+			yield return new WaitForSeconds(1);
+		}
+	}
+	
+	void spawnCoin (int n)
+	{
+		if (n>0)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				Vector3 coinPosition = new Vector3 (Random.Range (-10f, 10f), Random.Range (-3f, 3f), 0f);
+				Instantiate (coin, coinPosition, Quaternion.identity);
+			}
+		}
+	}
+}

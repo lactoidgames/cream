8644b9395985d622ec216f52ef29fc22a9f3084c
Ben Holyomes <bholyomes@gmail.com>
2015-02-08 14:32:32 -0800

    added main menu buttons

--- Assets/Scripts/UIManagerScript.cs
+++ Assets/Scripts/UIManagerScript.cs
@@ -3,8 +3,36 @@ using System.Collections;
 
 public class UIManagerScript : MonoBehaviour {
 
-	public void StartGame()
+	public GameObject newGameBtn;
+	public GameObject settingsBtn;
+	public GameObject randomBtn;
+	public GameObject friendsBtn;
+	public GameObject timeBtn;
+	public GameObject backBtn;
+	
+	public void NewGame()
+	{
+		newGameBtn.SetActive (false);
+		settingsBtn.SetActive (false);
+		randomBtn.SetActive (true);
+		friendsBtn.SetActive (true);
+		timeBtn.SetActive (true);
+		backBtn.SetActive (true);
+	}
+
+	public void MainMenu()
+	{
+		newGameBtn.SetActive (true);
+		settingsBtn.SetActive (true);
+		randomBtn.SetActive (false);
+		friendsBtn.SetActive (false);
+		timeBtn.SetActive (false);
+		backBtn.SetActive (false);
+	}
+
+	public void StartTimeTrial()
 	{
 		Application.LoadLevel ("Main");
 	}
+
 }

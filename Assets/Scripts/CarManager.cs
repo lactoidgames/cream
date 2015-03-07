b0b18762d16bd208725d5aebe03cb53c3ec6e724
Ben Holyomes <bholyomes@gmail.com>
2015-03-06 19:28:36 -0800

    added car spawning, needs artwork

---
+++ Assets/Scripts/CarManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarManager : MonoBehaviour {
+
+	public float speed;
+
+	void Update()
+	{
+		if (this.transform.position.y == 1.5f || this.transform.position.y == -1.85f)
+		{
+			Vector3 right = new Vector3(0.5f, 0f);
+			this.transform.Translate (right * speed * Time.deltaTime);
+		}
+
+		if (this.transform.position.y == 2.1f || this.transform.position.y == -1.2f)
+		{
+			Vector3 left = new Vector3(-0.5f, 0f);
+			this.transform.Translate (left * speed * Time.deltaTime);
+		}
+
+		if (this.transform.position.x > 6f || this.transform.position.x < -6f)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,12 +24,44 @@ public class GameManager : MonoBehaviour {
 
 		mainManager.spawnCoin (50);
 		StartCoroutine (mainManager.SpawnCoins ());
+
+		waypoints = new GameObject[] {waypoint1, waypoint2, waypoint3, waypoint4};
+		StartCoroutine(SpawnCars ());
 	}
 
 	void Update()
 	{
 		mainManager.timeText.text = mainManager.time.ToString();
 	}
+
+	public GameObject waypoint1;
+	public GameObject waypoint2;
+	public GameObject waypoint3;
+	public GameObject waypoint4;
+	public GameObject[] waypoints;
+	public float speed;
+
+	IEnumerator SpawnCars()
+	{
+		while (true)
+		{
+			foreach(GameObject w in waypoints)
+			{
+				GameObject car = Instantiate(Resources.Load("carplaceholder")) as GameObject;
+				car.transform.position = w.transform.position;
+				car.name = "car";
+				if (w.tag == "left")
+				{
+					Vector3 newScale = car.transform.localScale;
+					newScale.x *= -1;
+					car.transform.localScale = newScale;
+				}
+				yield return new WaitForSeconds(1);
+			}
+			yield return new WaitForSeconds(2);
+		}
+	}
+		                          
 
 
 }

a85832e93317c39348544d8955c45e4bf69ef1e0
Ben Holyomes <bholyomes@gmail.com>
2015-03-23 19:31:59 -0700

    updated buildings to become transparant when player is behind them

---
+++ Assets/Scripts/AlphaAdjuster.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlphaAdjuster : MonoBehaviour {
+
+	private SpriteRenderer spriteRenderer; 
+	
+	void Start()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		if(c.gameObject.tag == "player")
+		{
+			spriteRenderer.color = new Color(spriteRenderer.color.r,spriteRenderer.color.g,spriteRenderer.color.b,0.5f);
+		}
+	}
+		   
+		   
+	void OnTriggerExit2D(Collider2D c)
+	{			
+		if(c.gameObject.tag == "player")
+		{
+			spriteRenderer.color = new Color(spriteRenderer.color.r,spriteRenderer.color.g,spriteRenderer.color.b,1f);
+		}
+	}
+}
